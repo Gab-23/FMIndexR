f9e0a6b41aa85d166a81eb42528946cab33a53c5
Gabriele Oliveto <gabrieleoliveto26@gmail.com>
2024-08-08 10:56:06 +0200

    Update 0808241056

--- R/BackwardSearch.R
+++ R/BackwardSearch.R
@@ -1,11 +1,17 @@
 #' BackwardSearch
 #'
-#' Looks for patterns inside the original sequence, using its FM_index and performing a backward search procedure for finding patterns
+#' Looks for patterns inside the original sequence,
+#' using its FM_index and performing a
+#' backward search procedure for finding patterns
 #'
 #' @param FM_index object of class FM_index obtained using FM_index_from_FASTA
 #' @param pattern string containing the pattern to look for
-#' @param store_elems default = FALSE, can be modified to save the original sequence, the pattern indexes and the pattern in case a manual check wants to be made
-#' @return Prints a report of the number of patterns and their location, if store_elems = TRUE a report for manual check can be returned
+#' @param store_elems default = FALSE, can be modified to save
+#'     the original sequence,
+#'     the pattern indexes and the pattern
+#'     in case a manual check wants to be made
+#' @return A report of the number of patterns and their location,
+#'     if store_elems = TRUE a report for manual check can be returned
 #' @importFrom IRanges reverse
 #' @importFrom methods is
 #' @examples
@@ -13,76 +19,79 @@
 #' input_file <- system.file("extdata", "prova.txt", package = "FMIndexR")
 #' output_path <- system.file("output", package = "FMIndexR")
 #' FM_index <- FM_index_from_FASTA(input_file, output_path, save = FALSE)
-#' BackwardSearch(FM_index,"ATC")
+#' BackwardSearch(FM_index, "ATC")
 #' @export
 BackwardSearch <- function(FM_index, pattern, store_elems = FALSE) {
-
-  if (!is(pattern,'character') || (nchar(pattern) == 0)) {
-    stop('ERROR! Pattern MUST be a non-empty string')
+  if (!is(pattern, "character") || (nchar(pattern) == 0)) {
+    stop("ERROR! Pattern MUST be a non-empty string")
   } else if (!is(FM_index, "FM_index")) {
-      stop('ERROR! FM index MUST be of class FM_index!')
+    stop("ERROR! FM index MUST be of class FM_index!")
+  } else {
+    SA <- FM_index$SuffixArray
+    BWT <- FM_index$BWT
+    Occ <- FM_index$Occ
+    C <- FM_index$CountArray
+
+    SA$no_money <- substr(SA$suffix, 1, nchar(SA$suffix) - 1)
+
+    original_sequence <- SA[SA$idx == 0, ]$suffix
+    original_sequence <- substr(original_sequence,
+                                1,
+                                nchar(original_sequence) - 1)
+    original_sequence_array <- strsplit(original_sequence, split = "")[[1]]
+
+    pattern <- toupper(pattern)
+    reversed_pattern <- IRanges::reverse(pattern)
+    reversed_pattern_array <- strsplit(reversed_pattern, split = "")[[1]]
+
+    logical_2 <- length(setdiff(reversed_pattern_array,
+                                original_sequence_array)) > 0
+
+    if (logical_2) {
+      message("Pattern NOT found")
+      return(NULL)
+    }
+
+    if (any(SA$no_money == pattern)) {
+      match <- which(SA$no_money == pattern)
+      match <- rownames(SA)[match]
     } else {
+      match <- ""
+    }
 
-      SA <- FM_index$SuffixArray
-      BWT <- FM_index$BWT
-      Occ <- FM_index$Occ
-      C <- FM_index$CountArray
+    start <- 1
+    end <- (nchar(BWT) - 1)
 
-      SA$no_money <-substr(SA$suffix, 1, nchar(SA$suffix) - 1)
+    for (char in reversed_pattern_array) {
+      start <- C[as.character(0), char] + Occ[as.character(start - 1), char]
+      end <- C[as.character(0), char] + Occ[as.character(end), char] - 1
 
-      original_sequence <- SA[SA$idx == 0,]$suffix
-      original_sequence <- substr(original_sequence, 1, nchar(original_sequence) - 1)
-      original_sequence_array <- strsplit(original_sequence, split = '')[[1]]
-
-      pattern <- toupper(pattern)
-      reversed_pattern <- IRanges::reverse(pattern)
-      reversed_pattern_array <- strsplit(reversed_pattern, split = '')[[1]]
-
-      logical_2 <- length(setdiff(reversed_pattern_array,original_sequence_array)) > 0
-
-      if (logical_2) {
-        message('Pattern NOT found')
+      if ((start > end) & (nchar(match) == 0)) {
+        message("Pattern NOT found")
         return(NULL)
+        break
       }
+    }
 
-      if (any(SA$no_money == pattern)) {
-        match <- which(SA$no_money == pattern)
-        match <- rownames(SA)[match]
-      } else {match <- ''}
+    if (nchar(match) != 0) {
+      start <- as.integer(match)
+    }
 
-      start <- 1
-      end <- (nchar(BWT)-1)
+    final_range <- as.character((start):(end))
 
-      for (char in reversed_pattern_array) {
+    num_pattern <- length(final_range)
+    indexes <- sort(as.vector(SA[final_range, ]$idx))
+    indexes_str <- paste(indexes, collapse = ", ")
 
-        start <- C[as.character(0),char] + Occ[as.character(start-1),char]
-        end <- C[as.character(0),char] + Occ[as.character(end),char] -1
+    if (nchar(original_sequence) <= 3000) {
+      message(sprintf("Original Sequence: %s", original_sequence))
+    }
 
-        if ((start > end) & (nchar(match) == 0)) {
-          message('Pattern NOT found')
-          return(NULL)
-          break
-        }
-      }
+    message(sprintf("%d pattern(s) found", num_pattern))
+    message(sprintf("Index(es): %s ", indexes_str))
 
-      if (nchar(match) != 0) {
-        start <- as.integer(match)}
-
-      final_range <- as.character((start):(end))
-
-      num_pattern <- length(final_range)
-      indexes <- sort(as.vector(SA[final_range,]$idx))
-      indexes_str <- paste(indexes, collapse = ", ")
-
-      if (nchar(original_sequence) <= 3000) {
-        message(sprintf('Original Sequence: %s', original_sequence))
-      }
-
-      message(sprintf('%d pattern(s) found',num_pattern))
-      message(sprintf('Index(es): %s ', indexes_str))
-
-      if (store_elems) {
-        return(list(original_sequence,indexes,pattern))
-      }
-
-    }}
+    if (store_elems) {
+      return(list(original_sequence, indexes, pattern))
+    }
+  }
+}
--- R/FM_index_from_FASTA.R
+++ R/FM_index_from_FASTA.R
@@ -2,19 +2,13 @@
 #'
 #' Generates FM_index structure starting from an input FASTA file
 #'
-#' FM index is composed of 4 different elements:
-#'
-#' - Suffix Array --> stored as a Dataframe, contains all suffixes and their indexes of the original string
-#' - Burrows-Wheeler Transform --> a transformed string
-#' - Occurrency matrix --> Dataframe storing the cumulative count table of characters in the BWT
-#' - Count array --> Dataframe containing, for each character, the count of characters that are lexicographically smaller in the sorted BWT
-#'
-#' The FASTA file is parsed using a Biostrings function (readDNAStringSet) in order to ensure a robust and reliable parsing method
-#' The FM Index returned by the function contains also the header of the FASTA file in order to keep informations related to the original sequence
+#' The FASTA file is parsed using a Biostrings function (readDNAStringSet)
+#' in order to ensure a robust and reliable parsing method
 #'
 #' @param input path to the FASTA file
 #' @param output path to the folder in which to save the data structures
-#' @param save default = TRUE, can be modified to avoid saving data in the output path as separate files
+#' @param save default = TRUE, can be modified
+#'     to avoid saving data in the output path as separate files
 #' @return Object of type FM_index (list-like object)
 #' @importFrom zoo na.locf
 #' @importFrom Biostrings readDNAStringSet
@@ -26,67 +20,75 @@
 #' FM_index <- FM_index_from_FASTA(input_file, output_path, save = FALSE)
 #' FM_index
 #' @export
-FM_index_from_FASTA <- function(input, output, save = TRUE){
-
+FM_index_from_FASTA <- function(input, output, save = TRUE) {
   SuffixArray <- function(input_string) {
     if (nchar(input_string) == 0) {
-      stop('ERROR! Empty sequence detected!')
+      stop("ERROR! Empty sequence detected!")
     } else {
-      special_char <- '$'
-      complete_string <- paste(input_string,special_char, sep = '', collapse = '')
+      special_char <- "$"
+      complete_string <- paste(input_string,
+                               special_char,
+                               sep = "", collapse = "")
       complete_string_length <- nchar(complete_string)
 
-      idx_array <- 0:(complete_string_length-1)
+      idx_array <- 0:(complete_string_length - 1)
 
-      suffix_array <- vapply(X = idx_array, function(i){
+      suffix_array <- vapply(X = idx_array, function(i) {
         actual_idx <- i
         used_idx <- i + 1
-        suffix <- substr(complete_string, used_idx,complete_string_length)}, character(1))
+        suffix <- substr(complete_string, used_idx, complete_string_length)
+      }, character(1))
 
-      suffix_df <- data.frame(idx = idx_array,
-                              suffix = suffix_array)
+      suffix_df <- data.frame(
+        idx = idx_array,
+        suffix = suffix_array
+      )
 
-      sorted_suffix_df <- suffix_df[order(suffix_array),]
-      rownames(sorted_suffix_df) <- 0:(nrow(sorted_suffix_df)-1)
+      sorted_suffix_df <- suffix_df[order(suffix_array), ]
+      rownames(sorted_suffix_df) <- 0:(nrow(sorted_suffix_df) - 1)
       return(sorted_suffix_df)
-    }}
-  BWTransform <- function(suffix_array) {
-    original_sequence <- suffix_array[suffix_array$idx == 0,2]
-    actual_idx <- suffix_array$idx
-    used_idx <- ifelse(actual_idx == 0, nchar(original_sequence),actual_idx)
-
-    BWT <- vapply(X = used_idx, function(i){
-                  idx <- i
-                  substr(original_sequence,idx,idx)}, character(1))
-
-    BWT <- paste(BWT, sep = '', collapse = '')
-    return(BWT)
     }
+  }
+  BWTransform <- function(suffix_array) {
+    original_sequence <- suffix_array[suffix_array$idx == 0, 2]
+    actual_idx <- suffix_array$idx
+    used_idx <- ifelse(actual_idx == 0, nchar(original_sequence), actual_idx)
+    BWT <- vapply(X = used_idx, function(i) {
+      idx <- i
+      substr(original_sequence, idx, idx)
+    }, character(1))
+
+    BWT <- paste(BWT, sep = "", collapse = "")
+    return(BWT)
+  }
   OccMatrix <- function(bwt) {
     length_bwt <- nchar(bwt)
-    bwt_letters <- strsplit(bwt, split = '')[[1]]
+    bwt_letters <- strsplit(bwt, split = "")[[1]]
     unique_bwt_letters <- unique(bwt_letters)
     sorted_unique_bwt_letters <- sort(unique_bwt_letters)
 
-    Occ_df <- data.frame(matrix(data = NA, nrow = length_bwt, ncol = length(sorted_unique_bwt_letters)))
+    Occ_df <- data.frame(matrix(data = NA,
+                                nrow = length_bwt,
+                                ncol = length(sorted_unique_bwt_letters)))
     colnames(Occ_df) <- sorted_unique_bwt_letters
 
     for (char in colnames(Occ_df)) {
       idx_vector <- which(bwt_letters == char)
-      Occ_df[idx_vector,char] <- seq_along(idx_vector)
+      Occ_df[idx_vector, char] <- seq_along(idx_vector)
 
-      count_vec <- Occ_df[,char]
+      count_vec <- Occ_df[, char]
       count_vec <- zoo::na.locf(count_vec, na.rm = FALSE)
       count_vec[is.na(count_vec)] <- 0
 
-      Occ_df[,char] <- count_vec}
-
-    rownames(Occ_df) <- 0:(nrow(Occ_df)-1)
-    return(Occ_df)
+      Occ_df[, char] <- count_vec
     }
+
+    rownames(Occ_df) <- 0:(nrow(Occ_df) - 1)
+    return(Occ_df)
+  }
   CountArray <- function(bwt) {
-    bwt_char <- sort(unique((strsplit(bwt, split = '')[[1]])))
-    sorted_bwt <- sort(strsplit(bwt, split = '')[[1]])
+    bwt_char <- sort(unique((strsplit(bwt, split = "")[[1]])))
+    sorted_bwt <- sort(strsplit(bwt, split = "")[[1]])
 
     count_df <- data.frame(matrix(data = NA, nrow = 1, ncol = length(bwt_char)))
     colnames(count_df) <- bwt_char
@@ -94,54 +96,74 @@ FM_index_from_FASTA <- function(input, output, save = TRUE){
     for (char in bwt_char) {
       df_idx <- which(colnames(count_df) == char)
       char_idx <- which(sorted_bwt == char)[1] - 1
-      count_df[1,df_idx] <- char_idx
-      }
+      count_df[1, df_idx] <- char_idx
+    }
 
     rownames(count_df) <- 0
     return(count_df)
-    }
+  }
 
   fasta_data <- Biostrings::readDNAStringSet(input)
 
   if (length(fasta_data) > 1) {
-    stop('ERROR! multiFASTA files are not accepted!')
+    stop("ERROR! multiFASTA files are not accepted!")
   } else {
-
     fasta_sequence <- as.character(fasta_data)
     fasta_sequence <- toupper(fasta_sequence)
 
     fasta_sequence_header <- names(fasta_data)
-    fasta_sequence_header_path <- paste(output, 'name.txt', sep = '', collapse = '')
-    message('FASTA sequence correctly read!')
+    fasta_sequence_header_path <- paste(output,
+                                        "name.txt",
+                                        sep = "", collapse = "")
+    message("FASTA sequence correctly read!")
 
     suffix_array <- SuffixArray(fasta_sequence)
-    suffix_array_path <- paste(output, 'suffix_array.txt', sep = '', collapse = '')
-    message('Suffix Array created!')
+    suffix_array_path <- paste(output,
+                               "suffix_array.txt",
+                               sep = "", collapse = "")
+    message("Suffix Array created!")
 
     BWT <- BWTransform(suffix_array)
-    BWT_path <- paste(output, 'BWT.txt', sep = '', collapse = '')
-    message('BWT created!')
+    BWT_path <- paste(output, "BWT.txt", sep = "", collapse = "")
+    message("BWT created!")
 
     occ_matrix <- OccMatrix(BWT)
-    occ_matrix_path <- paste(output, 'occ_matrix.txt', sep = '', collapse = '')
-    message('Occ matrix created!')
+    occ_matrix_path <- paste(output, "occ_matrix.txt", sep = "", collapse = "")
+    message("Occ matrix created!")
 
     c_array <- CountArray(BWT)
-    c_array_path <- paste(output, 'c_array.txt', sep = '', collapse = '')
-    message('C Array created!')
+    c_array_path <- paste(output, "c_array.txt", sep = "", collapse = "")
+    message("C Array created!")
 
     if (save) {
       writeLines(fasta_sequence_header, con = fasta_sequence_header_path)
-      utils::write.table(suffix_array, file = suffix_array_path, sep = '\t', row.names = FALSE)
+      utils::write.table(suffix_array,
+                         file = suffix_array_path,
+                         sep = "\t",
+                         row.names = FALSE)
       writeLines(BWT, con = BWT_path)
-      utils::write.table(occ_matrix, file = occ_matrix_path, sep = '\t', row.names = FALSE)
-      utils::write.table(c_array, file = c_array_path, sep = '\t', row.names = FALSE)
+      utils::write.table(occ_matrix,
+                         file = occ_matrix_path,
+                         sep = "\t",
+                         row.names = FALSE)
+      utils::write.table(c_array,
+                         file = c_array_path,
+                         sep = "\t",
+                         row.names = FALSE)
     }
 
-    FM_index <- list(fasta_sequence_header,suffix_array,BWT,occ_matrix,c_array)
-    names(FM_index) <- c('SequenceName','SuffixArray', 'BWT', 'Occ', 'CountArray')
-    class(FM_index) <- 'FM_index'
+    FM_index <- list(fasta_sequence_header,
+                     suffix_array,
+                     BWT,
+                     occ_matrix,
+                     c_array)
+    names(FM_index) <- c("SequenceName",
+                         "SuffixArray",
+                         "BWT",
+                         "Occ",
+                         "CountArray")
+    class(FM_index) <- "FM_index"
 
     return(FM_index)
-
-  }}
+  }
+}
