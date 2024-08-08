268398fd2deef7fe6eb124570091539c630d1fb4
Gabriele Oliveto <gabrieleoliveto26@gmail.com>
2024-08-08 12:22:10 +0200

    Update 0808241222

---
+++ tests/testthat/test-BackwardSearch.R
@@ -0,0 +1,30 @@
+library(testthat)
+library(FMIndexR)
+
+input_file <- system.file("extdata", "prova.txt", package = "FMIndexR")
+output_path <- system.file("output", package = "FMIndexR")
+FM_index <- suppressMessages(FM_index_from_FASTA(input_file, output_path, save = FALSE))
+not_an_FM_index <- 2
+not_a_pattern <- 2
+
+example_search <- suppressMessages(BackwardSearch(FM_index, 'ATC', TRUE))
+example_search_2 <- suppressMessages(BackwardSearch(FM_index, 'CCCCCCCCC', TRUE))
+
+test_that("Error with no FM_index", {
+  expect_error(BackwardSearch(not_an_FM_index, 'AGA'))})
+
+test_that("Error with empty pattern", {
+  expect_error(BackwardSearch(FM_index, ''))})
+
+test_that("Error with no pattern", {
+  expect_error(BackwardSearch(FM_index, not_a_pattern))})
+
+test_that("Found pattern", {
+  expect_equal(example_search[[2]], c(20,33))})
+
+test_that("Pattern not found", {
+  expect_null(example_search_2)})
+
+
+
+
---
+++ tests/testthat/test-FM_index_from_FASTA.R
@@ -0,0 +1,37 @@
+library(testthat)
+library(FMIndexR)
+
+input_file_ok <- system.file("extdata", "prova.txt", package = "FMIndexR")
+input_file_no_header <- system.file("extdata", "prova_no_header.txt", package = "FMIndexR")
+input_file_warning <- system.file("extdata", "prova_warning.txt", package = "FMIndexR")
+input_file_seq_vuota <- system.file("extdata", "prova_seq_vuota.txt", package = "FMIndexR")
+
+output_path <- system.file("output", package = "FMIndexR")
+
+FM_index_ok <- suppressMessages(FM_index_from_FASTA(input_file_ok,
+                                                    output_path,
+                                                    save = FALSE))
+
+test_that("Correct FM_index", {
+  expect_s3_class(FM_index_ok, "FM_index")})
+
+test_that("Error with no header", {
+  expect_error(FM_index_from_FASTA(input_file_no_header,
+                                   output_path,
+                                   save = FALSE))})
+
+test_that("Error with seq vuota", {
+  expect_error(suppressMessages(FM_index_from_FASTA(input_file_seq_vuota,
+                                   output_path,
+                                   save = FALSE)))})
+
+test_that("Warning with strange characters", {
+  expect_warning(suppressMessages(FM_index_from_FASTA(input_file_warning,
+                                                      output_path,
+                                                      save = FALSE)))})
+
+
+
+
+
+
