4e3d6d64a270fb3ff9fd5810525bc1fc46452b29
Gabriele Oliveto <gabrieleoliveto26@gmail.com>
2024-08-10 10:00:09 +0200

    Update 1008241000

--- tests/testthat/test-FM_index_from_FASTA.R
+++ tests/testthat/test-FM_index_from_FASTA.R
@@ -5,13 +5,13 @@ input_file_ok <- system.file("extdata", "prova.txt", package = "FMIndexR")
 input_file_no_header <- system.file("extdata", "prova_no_header.txt", package = "FMIndexR")
 input_file_warning <- system.file("extdata", "prova_warning.txt", package = "FMIndexR")
 input_file_seq_vuota <- system.file("extdata", "prova_seq_vuota.txt", package = "FMIndexR")
+input_file_multifasta <- system.file("extdata", "prova_multifasta.txt", package = "FMIndexR")
 
 output_path <- system.file("output", package = "FMIndexR")
 
 FM_index_ok <- suppressMessages(FM_index_from_FASTA(input_file_ok,
-  output_path,
-  save = FALSE
-))
+                                                    output_path,
+                                                    save = FALSE))
 
 test_that("Correct FM_index", {
   expect_s3_class(FM_index_ok, "FM_index")
@@ -37,3 +37,12 @@ test_that("Warning with strange characters", {
     save = FALSE
   )))
 })
+
+test_that("Error with multifasta", {
+  expect_error(FM_index_from_FASTA(input_file_multifasta,
+                                   output_path,
+                                   save = FALSE
+  ))
+})
+
+
