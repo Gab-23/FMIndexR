1ed269f069f17c0204e9fdcf974d471783e49e39
Gabriele Oliveto <gabrieleoliveto26@gmail.com>
2024-08-21 19:17:27 +0200

    Update zzz

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+#' @title FMIndexR: Easy FM Index Creation and Pattern Search from FASTA
+#'
+#' @description
+#' FMIndexR provides the means to generate an FM index starting from a FASTA
+#' file. The user will be able to save the individual data structures
+#' composing it. The FM index will then be used to look for patterns inside
+#' the sequence using Backward Search
+#'
+#' It includes functions for the creation of the FM index and the search of
+#' patterns.
+#'
+#'
+#' @name FMIndexR
+#' @keywords package
+"_PACKAGE"
