79b42c77869e11820159496603be6f3da11779c4
Gabriele Oliveto <gabrieleoliveto26@gmail.com>
2024-08-22 10:35:20 +0200

    Update file doc

---
+++ inst/script/Document-extdata.R
@@ -0,0 +1,41 @@
+# inst/script/Document_extdata.R
+
+# File: size-time-plot.png
+# Purpose: Image that will be shown in the vignette
+# Usage: Loaded in the vignette
+
+# File: prova.txt
+# Purpose: FASTA file used in tests and function manual
+# Usage: Synthetic sequence to represent a working FASTA file
+
+# File: prova_multifasta.txt
+# Purpose: multiFASTA file to be used in tests
+# Usage: Synthetic sequence to represent a non-working FASTA file
+
+# File: prova_no_header.txt
+# Purpose: FASTA file with no header to be used in tests
+# Usage: Synthetic sequence to represent a non-working FASTA file
+
+# File: prova_seq_vuota.txt
+# Purpose: FASTA file without a sequence to be used in tests
+# Usage: Synthetic sequence to represent a non-working FASTA file
+
+# File: prova_vignette.txt
+# Purpose: FASTA file used in vignette
+# Usage: Synthetic sequence to represent a working FASTA file
+
+# File: prova_warning.txt
+# Purpose: FASTA file containing characters rejected by Biostrings::readDNAStringSet
+# Usage: Synthetic sequence to represent a problematic FASTA file
+
+# File: insulin_FASTA.txt
+# Purpose: Real world example of FASTA file (NM_001185098.2)
+# Usage: Sequence provided to the user to be used as real world example
+
+# File: SHH_FASTA.txt
+# Purpose: Real world example of FASTA file (NR_132319.2)
+# Usage: Sequence provided to the user to be used as real world example
+
+# File: SNCB_FASTA.txt
+# Purpose: Real world example of FASTA file (NM_001001502.3)
+# Usage: Sequence provided to the user to be used as real world example
